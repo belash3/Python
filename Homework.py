7fd580113d945d42f02bb2f4c05b80fffcb1130c
Sergey <belash3@mail.ru>
2023-12-20 22:54:48 +0300

    Remove json and add SQLite in phonebook

--- Homework.py
+++ Homework.py
@@ -1,89 +1,94 @@
-import json
+import sqlite3 as sl
+
+
+
 
 def add():
     print(" ")
     name = input("Введите имя: ")
-    phones = input("Введите телефоны через пробел: ").split(" ")
+    phones = input("Введите телефоны через пробел: ")
     birthday = input("Введите дату рождения: ")
     email = input("Введите email: ")
-    contact = {'phones': phones,
-                        'birthday': birthday, 'email': email}
-    phonebook[name] = contact
-    print(contact)
+    cur.execute(f"INSERT INTO users VALUES ('{name}', '{phones}', '{birthday}', '{email}');")
     print("Контакт добавлен")
     print()
-    
-    
-def save():
-    with open("contants.json", "w", encoding="utf-8") as fh:
-        fh.write(json.dumps(phonebook, ensure_ascii=False))
+
         
 def show():
-    if len(phonebook) != 0:
-        for name, values in phonebook.items():
-            print(name, values)
-    else:
-        print("Нет ни одного контакта! ")
+    cur.execute("SELECT name FROM users;")
+    print(cur.fetchall())
 
 def edit():
+    show()
     contact_name = input("Введите имя контакта для изменения: ")
     try:
-        print(f"Контакт для изменения: {contact_name}")
-        print(phonebook[contact_name])
+        cur.execute(f"SELECT * FROM users WHERE name = '{contact_name}';")
+        print(f"Контакт для изменения: {cur.fetchall()}")
         new_name = input("Введите новое имя: ")
         if new_name != "":
-            phonebook[new_name] = phonebook[contact_name]
-            del phonebook[contact_name]
+            cur.execute(f"UPDATE users SET name = '{new_name}' WHERE name = '{contact_name}';")
             contact_name = new_name
-        new_phones = input("Введите телефоны через пробел: ").split(" ")
+        new_phones = input("Введите телефоны через пробел: ")
         if new_phones != "":
-            phonebook[contact_name]['phones'] = new_phones
+            cur.execute(f"UPDATE users SET phones = '{new_phones}' WHERE name = '{contact_name}';")
         new_birthday = input("Введите дату рождения: ")
         if new_birthday != "":
-            phonebook[contact_name]['birthday'] = new_birthday
+            cur.execute(f"UPDATE users SET birthday = '{new_birthday}' WHERE name = '{contact_name}';")
         new_email = input("Введите email: ")
         if new_email != "":
-            phonebook[contact_name]['email'] = new_email       
-        save()
-
+            cur.execute(f"UPDATE users SET email = '{new_email}' WHERE name = '{contact_name}';")
     except:
         print("Такого контакта нет")
     
 def delete():
+    show()
     contact = input("Введите имя контакта для удаления: ")
     try:
-        print(phonebook[contact])
         choice = input("Введите 'Y' если уверены:  ")
         if choice == "Y":
-            del phonebook[contact]
+            cur.execute(f"DELETE FROM users WHERE name = '{contact}';")
             print("Контакт удален")
-            save()
     except:
         print("Такого контакта нет")
 
-def load():
-    try:
-        with open("contants.json", "r", encoding="utf-8") as fh:
-            contacts = json.loads(fh.read())
-    except:
-        print("Нет контактов, добавьте хотя бы один [add]")
-        contacts = None
-    return contacts
 
-phonebook = load()
+def search():
+    searched_name = input("Введите имя для поиска: ")
+    cur.execute(f"SELECT * FROM users WHERE name = '{searched_name}';")
+    print(cur.fetchall())
 
-while True:
-    print("Вам доступны команды: add, save, show, edit, delete")
+
+conn = sl.connect("phonebook.db")
+cur = conn.cursor()
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS users
+            (
+            name TEXT PRIMARY KEY,
+            phones TEXT,
+            birthday TEXT,
+            email TEXT
+            );
+            """)
+
+
+Flag = True
+while Flag:
+    print("Вам доступны команды: add, show, search, edit, delete. \nДля сохранения изменений и выхода введите exit.")
     command = input("Введите команду: ")
     if command == "add":
         add()
-    elif command == "save":
-        save()
     elif command == "show":
         show()
+    elif command == "search":
+        search()
     elif command == "delete":
         delete()
     elif command == "edit":
         edit()
+    elif command == "exit":
+        Flag = False
     else:
         print("Неверная команда")
+
+conn.commit()
+conn.close()
